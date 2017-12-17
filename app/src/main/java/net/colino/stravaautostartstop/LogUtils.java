75bca536497bcd3e103fbd2a16d95d70e42074f3
Colin Leroy <colin@colino.net>
2017-12-17 13:42:33 +0100

    Build fixes

--- app/src/main/java/net/colino/stravaautostartstop/LogUtils.java
+++ app/src/main/java/net/colino/stravaautostartstop/LogUtils.java
@@ -3,17 +3,17 @@ import android.util.Log;
 
 public class LogUtils {
     public static void d(final String tag, String message) {
-        if (BuildConfig.DEBUG) {
+        if (BuildConfig.BUILD_TYPE.equals("debug")) {
             Log.d(tag, message);
         }
     }
     public static void i(final String tag, String message) {
-        if (BuildConfig.DEBUG) {
+        if (BuildConfig.BUILD_TYPE.equals("debug")) {
             Log.i(tag, message);
         }
     }
     public static void e(final String tag, String message) {
-        if (BuildConfig.DEBUG) {
+        if (BuildConfig.BUILD_TYPE.equals("debug")) {
             Log.e(tag, message);
         }
     }
